7909b734d9d48913c2049411dfc6e95ade93f0be
Jeremy Mi <mizhemin@amazon.com>
2016-07-12 21:18:24 -0700

    Add robust unit tests for selection sort.

--- tst/com/forevermzm/algorithms/SelectionSortTest.java
+++ tst/com/forevermzm/algorithms/SelectionSortTest.java
@@ -2,7 +2,9 @@ package com.forevermzm.algorithms;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -16,5 +18,28 @@ public class SelectionSortTest {
         SelectionSort.sort(elements);
 
         assertEquals(Arrays.asList(-8, -3, -1, 7, 8), elements);
+    }
+
+    @Test
+    public void testSort_withOneElement() {
+        List<Integer> elements = Arrays.asList(8);
+
+        SelectionSort.sort(elements);
+
+        assertEquals(Arrays.asList(8), elements);
+    }
+
+    @Test
+    public void testSort_withNoElement() {
+        List<Integer> elements = new ArrayList<>();
+
+        SelectionSort.sort(elements);
+
+        assertEquals(new ArrayList<Integer>(), elements);
+    }
+
+    @Test
+    public void testSort_withNull() {
+        SelectionSort.sort(null);
     }
 }
