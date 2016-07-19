8d404d9fcd804cf54de6e858bf3e0e840ede7844
Jeremy Mi <forevermzm@gmail.com>
2016-07-18 23:31:48 -0700

    Add basic binary search and a bunch of variants ways. All with unit tests.

---
+++ src/com/forevermzm/algorithms/BinarySearch.java
@@ -0,0 +1,115 @@
+package com.forevermzm.algorithms;
+
+public class BinarySearch {
+    public static int search(int[] elements, int target) {
+        if (elements == null || elements.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = elements.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (elements[mid] > target) {
+                right = mid - 1;
+            } else if (elements[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public static int searchFirstOccurrence(int[] elements, int target) {
+        if (elements == null || elements.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = elements.length - 1;
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (elements[mid] >= target) {
+                right = mid;
+            } else if (elements[mid] < target) {
+                left = mid;
+            }
+        }
+        if (elements[left] == target) {
+            return left;
+        } else if (elements[right] == target) {
+            return right;
+        }
+        return -1;
+    }
+
+    public static int searchLastOccurrence(int[] elements, int target) {
+        if (elements == null || elements.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = elements.length - 1;
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (elements[mid] <= target) {
+                left = mid;
+            } else if (elements[mid] > target) {
+                right = mid;
+            }
+        }
+        if (elements[right] == target) {
+            return right;
+        } else if (elements[left] == target) {
+            return left;
+        }
+        return -1;
+    }
+
+    public static int searchClosest(int[] elements, int target) {
+        if (elements == null || elements.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = elements.length - 1;
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (elements[mid] == target) {
+                return mid;
+            } else if (elements[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return Math.abs(elements[left] - target) <= Math.abs(elements[right] - target) ?
+                left : right;
+    }
+
+    public static int searchUnknownSize(int[] elements, int target, int jump) {
+        if (elements == null || elements.length == 0) {
+            return -1;
+        }
+        if (elements[0] > target) {
+            return -1;
+        }
+        int left = 0;
+        int right = jump;
+        while (elements.length > right
+                && elements[right] < target) {
+            left = left + jump;
+            right = right + jump;
+        }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (elements.length <= mid) {
+                right = mid - 1;
+            } else if (elements[mid] == target) {
+                return mid;
+            } else if (elements[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
